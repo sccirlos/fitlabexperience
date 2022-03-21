262cbd38c60b77223337f9ad1fd241d21e0f3c2b
sccirlos <sccirlos@gmail.com>
2022-03-21 12:43:31 -0500

    replacing nav bar

--- about.js
+++ about.js
@@ -1,1 +1,6 @@
-var nav = responsiveNav(".nav-collapse");
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
